2946cbf6c18e9dcddf9e44606a78d791c6d4d930
yhbird <yhbird1227@gmail.com>
2025-07-16 00:43:26 +0900

    init commit 20250716

---
+++ example.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+assert os.path.exists('token.env'), "token.env file not found"
+load_dotenv('token.env')
+test_bot_token: str = os.getenv('example_bot_token')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+test_bot = commands.Bot(command_prefix='/', intents=intents)
+
+@test_bot.event
+async def on_ready():
+    print(f'Logged in as {test_bot.user}!')
+
+@test_bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+test_bot.run(str(test_bot_token))
