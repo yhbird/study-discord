5f55f7ce8df2ac651da0149c9548cec308e1697c
yhbird <yhbird1227@gmail.com>
2025-07-24 03:58:53 +0900

    feat: 도움말 기능 추가

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import discord
 import gc
 from discord.ext import commands, tasks
+from datetime import datetime, timedelta
+
 from config import kst_format_now, get_memory_usage_mb
-from config import BOT_TOKEN_PRD, BOT_TOKEN_DEV, BOT_TOKEN_RUN, MEMORY_CLEAR_INTERVAL
+from config import BOT_TOKEN, MEMORY_CLEAR_INTERVAL, BOT_START_DT
 from service.common import logger
 
 # 디스코드 메세지 관련 명령어
@@ -13,16 +15,36 @@ import service.api_command as api_command
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='/', intents=intents)
-
+bot = commands.Bot(command_prefix='/', intents=intents, help_command=None)
 # 디버그용 명령어
 @bot.command(name="debug")
 async def bot_debug(ctx: commands.Context, arg: str = None):
-    await ctx.send(f"봇이 정상적으로 작동 중이에양! 현재 시간: {kst_format_now()}")
+    await ctx.send(f"[DEBUG] 봇이 정상적으로 작동 중이에양!")
     if arg == "mem":
         mem_usage: float = get_memory_usage_mb()
         logger.debug(f"Current memory usage: {mem_usage:.2f} MB")
         await ctx.send(f"현재 메모리 사용량: {mem_usage:.2f} MB")
+    if arg == "info":
+        bot_info: str = (
+            f"**봇 이름:** {bot.user.name}\n"
+            f"**봇 시작 시간:** {BOT_START_DT.strftime('%Y년 %m월 %d일 %H시 %M분 %S초')}"
+        )
+        now_dt: datetime = datetime.strptime(kst_format_now(), '%Y-%m-%d %H:%M:%S')
+        uptime: timedelta = now_dt - BOT_START_DT
+        # uptime의 일, 시간, 분, 초 계산
+        up_d: int = uptime.days
+        up_h: int = uptime.seconds // 3600
+        up_m: int = (uptime.seconds % 3600) // 60
+        up_s: int = uptime.seconds % 60
+        if up_d > 0:
+            debug_msg = f"bot uptime: {up_d}일 {up_h}시간 {up_m}분 {up_s}초"
+            send_msg = f"**봇 가동 시간:** {up_d}일 {up_h}시간 {up_m}분 {up_s}초"
+        else:
+            debug_msg = f"bot uptime {up_h}시간 {up_m}분 {up_s}초"
+            send_msg = f"**봇 가동 시간:** {up_h}시간 {up_m}분 {up_s}초"
+        logger.debug(debug_msg)
+        info_msg = f"{bot_info}\n{send_msg}"
+        await ctx.send(info_msg)
 
 # 1시간 마다 메모리 정리
 @tasks.loop(minutes=MEMORY_CLEAR_INTERVAL)
@@ -43,6 +65,10 @@ async def on_ready():
 async def run_msg_handle_blinkbang(ctx: commands.Context):
     await msg_command.msg_handle_blinkbang(ctx.message)
 
+@bot.command(name="help")
+async def run_msg_handle_help(ctx: commands.Context):
+    await msg_command.msg_handle_help(ctx.message)
+    
 # 명령어 등록 from service.api_command
 @bot.command(name="기본정보")
 async def run_api_basic_info(ctx: commands.Context, character_name: str):
@@ -71,5 +97,4 @@ async def on_message(message: discord.Message):
     await bot.process_commands(message)
 
 # 봇 실행!
-bot_token = BOT_TOKEN_PRD if BOT_TOKEN_RUN == "prd" else BOT_TOKEN_DEV
-bot.run(str(bot_token))
+bot.run(str(BOT_TOKEN))
